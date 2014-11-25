4bba3f25499534304f63b6c334485dae2507eb80
root <root@raspberrypi.(none)>
2014-11-25 21:30:56 +0000

    add rest of motor test code

--- src/main.c
+++ src/main.c
@@ -13,6 +13,32 @@ int main(int argc, char **argv)
 	INP_GPIO(15);
 	INP_GPIO(16);
 	INP_GPIO(18);
+	OUT_GPIO(7);
+	OUT_GPIO(11);
+	OUT_GPIO(12);
+	OUT_GPIO(13);
+	OUT_GPIO(15);
+	OUT_GPIO(16);
+	OUT_GPIO(18);
 
+	//Motor A direction
+	GPIO_SET = 1 << 12; //AIN1
+	GPIO_SET = 0 << 11; //AIN2
+	//Motor A speed
+	GPIO_SET = 1 << 7; //PWMA (No PWM yet :( )
+
+	//Motor B direction
+	GPIO_SET = 1 << 15; //BIN1
+	GPIO_SET = 0 << 16; //BIN2
+	//Motor B speed
+	GPIO_SET = 1 << 18; //PWMB (should change pin, this is HW PWM pin)
+
+	//Turn off standby by setting pin 13 high
+	GPIO_SET = 1 << 13;
+
+	//sleep
+
+	//TODO Camera, web access, sonar tests
+	
 	return 0;
 }
