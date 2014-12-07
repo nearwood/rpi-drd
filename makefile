acfa954a32ae93b11fb7292e643c1dbf6179512c
Nick <nearwood@gmail.com>
2014-12-06 23:43:35 -0500

    Switch to bcm2835 lib, and cross-compiling

--- makefile
+++ makefile
@@ -1,16 +1,20 @@
 
+PREFIX=arm-unknown-linux-gnueabi-
+INCLUDE=/home/nick/x-tools/arm-unknown-linux-gnueabi/include
+LIB=/home/nick/x-tools/arm-unknown-linux-gnueabi/lib
 SRC=$(wildcard src/*.c)
 OBJ=$(patsubst src/%.c,build/%.o,$(SRC))
 TARGET=bin/drd
+LDFLAGS=-I $(LIB)
 
 .PHONY: all clean upload
 
 all: $(TARGET)
-	strip $(TARGET)
-	@size -A $(TARGET) | grep "Total"
+	$(PREFIX)strip $(TARGET)
+	@$(PREFIX)size -A $(TARGET) | grep "Total"
 
 $(TARGET): $(OBJ) | bin
-	gcc $(LDFLAGS) $(OBJ) -o $@
+	$(PREFIX)gcc $(LDFLAGS) $(OBJ) -o $@
 
 $(OBJ): | build
 
@@ -21,7 +25,7 @@ build:
 	mkdir -p build
 
 build/%.o: src/%.c
-	gcc $(CFLAGS) -c $< -o $@
+	$(PREFIX)gcc $(CFLAGS) -I $(INCLUDE) -c $< -o $@
 
 upload: $(TARGET)
 	#scp
--- src/main.c
+++ src/main.c
@@ -1,44 +1,11 @@
 
-#include "gpio.h"
+//#include "gpio.h" //Direct register access
+#include <bcm2835.h>
+
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	gpio_setup();
-	//ALWAYS CALL INP_GPIO BEFORE USING OUT/SET!
-	//7,11,12,13,15,16,18
-	INP_GPIO(7);
-	INP_GPIO(11);
-	INP_GPIO(12);
-	INP_GPIO(13);
-	INP_GPIO(15);
-	INP_GPIO(16);
-	INP_GPIO(18);
-	OUT_GPIO(7);
-	OUT_GPIO(11);
-	OUT_GPIO(12);
-	OUT_GPIO(13);
-	OUT_GPIO(15);
-	OUT_GPIO(16);
-	OUT_GPIO(18);
-
-	//Motor A direction
-	GPIO_SET = 1 << 12; //AIN1
-	GPIO_SET = 0 << 11; //AIN2
-	//Motor A speed
-	GPIO_SET = 1 << 7; //PWMA (No PWM yet :( )
-
-	//Motor B direction
-	GPIO_SET = 1 << 15; //BIN1
-	GPIO_SET = 0 << 16; //BIN2
-	//Motor B speed
-	GPIO_SET = 1 << 18; //PWMB (should change pin, this is HW PWM pin)
-
-	//Turn off standby by setting pin 13 high
-	GPIO_SET = 1 << 13;
-
-	//sleep
-
-	//TODO Camera, web access, sonar tests
-	
+	printf("Hello world!\n");
 	return 0;
 }
