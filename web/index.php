0e6269af0ab072694ee48b91e2828905bbcfa51f
Nick <nearwood@gmail.com>
2015-06-23 22:49:26 -0400

    Back on project. Motors are *not* dead, neither is the controller. Battery case was shoddy. Vroom. PWM needs work.

---
+++ web/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DRD v0.1a</title>
+</head>
+<body>
+<h3>Status</h3>
+<h3>Control</h3>
+<form>
+<button>Forward</button>
+<button>Left</button><button>Right</button>
+<button>Reverse</button>
+</form>
+<h3>Cam</h3>
+<video autoplay="true" controls width="640" height="480">
+	<source src="http://192.168.0.104:8554/stream.mp4" />
+	Could not use HTML5 video.
+</video>
+</body>
+</html>
