bd6c2450893336388eefa9228a2ed289dd8c9a98
Nick <nearwood@gmail.com>
2019-09-18 12:03:19 -0500

    Attempt to encapsulate API calls

--- web/src/App.js
+++ web/src/App.js
@@ -4,21 +4,30 @@ import './App.css';
 import { ThemeProvider, createTheme, Content, Words, Header, Loading, Footer, Arwes } from 'arwes';
 import System from './System';
 
-const App = () => (
-  <ThemeProvider theme={createTheme()}>
-    <Arwes className="App">
-      <Content>
-        <Header animate>
-          <h1 style={{ margin: 0 }}>DRD</h1>
-        </Header>
-        <Words animate>Connecting... </Words><Loading animate/>
-        <System />
-        <Footer animate>
-          <h6>Web v0.00 / API v0.00 / Controller v0.00</h6>
-        </Footer>
-      </Content>
-    </Arwes>
-  </ThemeProvider>
-);
+import useApi from 'use-api';
+
+const App = () => {
+  const [{ data, isLoading, isError }, doFetch] = useApi(
+    'http://drd:4482/version',
+    { version: {} }
+  );
+
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Arwes className="App">
+        <Content>
+          <Header animate>
+            <h1 style={{ margin: 0 }}>DRD</h1>
+          </Header>
+          <Words animate>Connecting... </Words><Loading animate/>
+          <System />
+          <Footer animate>
+            <h6 onClick={() => doFetch()}>Web v0.00 / API v{isLoading ? '...' : data.version.api} / Controller v0.00</h6>
+          </Footer>
+        </Content>
+      </Arwes>
+    </ThemeProvider>
+  );
+};
 
 export default App;
