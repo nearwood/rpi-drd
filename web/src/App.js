059c25930925d483ba39183070a04dece94c3b4c
Nick <nearwood@gmail.com>
2019-09-17 22:40:41 -0500

    prototyping the ui up

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { ThemeProvider, createTheme, Arwes } from 'arwes';
-import { Button } from 'arwes';
+import { ThemeProvider, createTheme, Content, Words, Header, Loading, Footer, Arwes } from 'arwes';
+import System from './System';
 
 const App = () => (
   <ThemeProvider theme={createTheme()}>
-    <Arwes>
-      <h1>DRD</h1>
-      <Button>Shutdown</Button>
+    <Arwes className="App">
+      <Content>
+        <Header animate>
+          <h1 style={{ margin: 0 }}>DRD</h1>
+        </Header>
+        <Words animate>Connecting... </Words><Loading animate/>
+        <System />
+        <Footer animate>
+          <h6>Web v0.00 / API v0.00 / Controller v0.00</h6>
+        </Footer>
+      </Content>
     </Arwes>
   </ThemeProvider>
 );
---
+++ web/src/System.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Project, Appear, Button } from 'arwes';
+
+export default () => {
+  const [show, setShow] = useState(false);
+  return (
+    <Project animate header="System Controls">
+    <Button style={{float: 'left'}} animate layer='primary' onClick={() => setShow(!show)}>{show ? 'Hide' : 'Show'}</Button>
+      <Appear animate show={show}>
+        <Button animate layer='alert'>Shutdown</Button>
+      </Appear>
+    </Project>
+  );
+  }
