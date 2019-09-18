d055adeb7bd2f91b8a73ca0e08c443c3bc16fd8f
Nick <nearwood@gmail.com>
2019-09-17 22:41:21 -0500

    prototyping the ui up

--- web/src/System.jsx
+++ web/src/System.jsx
@@ -5,10 +5,10 @@ export default () => {
   const [show, setShow] = useState(false);
   return (
     <Project animate header="System Controls">
-    <Button style={{float: 'left'}} animate layer='primary' onClick={() => setShow(!show)}>{show ? 'Hide' : 'Show'}</Button>
+      <Button style={{float: 'left'}} animate layer='primary' onClick={() => setShow(!show)}>{show ? 'Hide' : 'Show'}</Button>
       <Appear animate show={show}>
         <Button animate layer='alert'>Shutdown</Button>
       </Appear>
     </Project>
   );
-  }
+}
