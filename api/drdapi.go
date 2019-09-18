9f9252ef8a5ccf0a80c4b566520bd65124077c39
Nick <nearwood@gmail.com>
2019-09-18 13:55:01 -0500

    Add basic API route

--- api/drdapi.go
+++ api/drdapi.go
@@ -1,41 +1,56 @@
 package main
 
 import (
-  "io"
-  "log"
-  "net"
-  "net/http"
-  "time"
-  "encoding/json"
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"net/http"
 )
 
+type Version struct {
+	Controller string `json:"controller"`
+	Api        string `json:"api"`
+}
+
+func apiVersion(w http.ResponseWriter, req *http.Request) {
+	version := Version{Controller: "0.1", Api: "1.0"}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(version)
+}
 
 func reader(r io.Reader) {
-  buf := make([]byte, 1024)
-  for {
-    n, err := r.Read(buf[:])
-    if err != nil {
-      return
-    }
-    println("Client got:", string(buf[0:n]))
-  }
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return
+		}
+		println("Client got:", string(buf[0:n]))
+	}
 }
 
 func main() {
-  c, err := net.Dial("unix", "/tmp/drd.sock")
-  if err != nil {
-    panic(err)
-  }
-  defer c.Close()
+	c, err := net.Dial("unix", "/tmp/drd.sock")
+	if err != nil {
+		//panic(err)
+	} else {
+		defer c.Close()
+	}
 
-  go reader(c)
-  for {
-    _, err := c.Write([]byte("hi"))
-    if err != nil {
-      log.Fatal("write error:", err)
-      break
-    }
-    time.Sleep(1e9)
-  }
+	//go reader(c)
+
+	http.HandleFunc("/version", apiVersion)
+
+	log.Println("Starting API server...")
+	log.Fatal(http.ListenAndServe(":4482", nil))
+	// for {
+	//   _, err := c.Write([]byte("hi"))
+	//   if err != nil {
+	//     log.Fatal("write error:", err)
+	//     break
+	//   }
+	//   time.Sleep(1e9)
+	//}
 }
-
