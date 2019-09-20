75dccd5437022f6c2ce8692a06d16097d7b1fd7f
Nick <nearwood@gmail.com>
2019-09-19 21:52:37 -0500

    Add layers, fix up api hooks

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider, createTheme, Content, Words, Header, Loading, Footer, Arwes } from 'arwes';
 import System from './System';
+import useApi from 'use-api';
 
 
 const App = () => {
+  const [{ data, isLoading, isError }] = useApi('http://drd:4482/version', false);
+
+  let connect = <Words animate layer='success'>Connected!</Words>;
+  if (isLoading) {
+    connect = <><Words animate>Connecting...</Words><Loading animate/></>;
+  } else if (isError) {
+    connect = <Words layer='alert'>Error connecting 😢</Words>;
+  }
   
   return (
     <ThemeProvider theme={createTheme()}>
@@ -14,9 +23,10 @@ const App = () => {
           <Header animate>
             <h1 style={{ margin: 0 }}>DRD</h1>
           </Header>
-          <Words animate>Connecting...</Words><Loading animate/>
-          <System />
+          {connect} 
+          <System disabled={!data}/>
           <Footer animate>
+            <h6>Web v0.00 {isLoading || isError ? '' : `API v${data.api} / Controller v${data.controller}`}</h6>
           </Footer>
         </Content>
       </Arwes>
--- web/src/System.jsx
+++ web/src/System.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
-import { Project, Appear, Button } from 'arwes';
+import { Project, Appear, Button, Words } from 'arwes';
 import useApi from 'use-api';
 
-export default () => {
-  const [{ data, isLoading, isError }, doFetch] = useApi(
-    null,
-    {}
-  );
+export default (props) => {
+  const [{ data, isLoading, isError }, api] = useApi();
+
+  const [disableControls, setDisabled] = useState(props.disabled);
   const [show, setShow] = useState(false);
+
+  const onShutdown = () => {
+    setDisabled(true);
+    api('http://drd:4482/shutdown');
+  };
+
   return (
     <Project animate header="System Controls">
       <Button style={{float: 'left'}} animate layer='primary' onClick={() => setShow(!show)}>{show ? 'Hide' : 'Show'}</Button>
       <Appear animate show={show}>
-        <h6>Web v0.00 / API v{isLoading ? '...' : data.api} / Controller v{data.controller}</h6>
-        <Button animate layer='alert' onClick={() => doFetch('http://drd:4482/version')}>Shutdown</Button>
+        <Button animate layer='alert' disabled={disableControls} onClick={onShutdown}>Shutdown</Button>
+        {isLoading && <Words animate>Shutting down... :(</Words>}
+        {isError === true && <Words animate layer='alert'>Error Shutting down!</Words>}
       </Appear>
     </Project>
   );
